818ff28cf24cb71d0b441fae625d737fee7a0d83
Alejandro <fernandez.goale21@triana.salesianos.edu>
2022-10-28 11:02:27 +0200

    commit en clase

--- dir/StarWarsComplete/StarWarsComplete/src/app/app.module.ts
+++ dir/StarWarsComplete/StarWarsComplete/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialImportsModule } from './modules/material-imports/material-imports.module';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
+import { FilmsListComponent } from './components/films-list/films-list.component';
+import { SpeciesListComponent } from './components/species-list/species-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersListComponent,
+    FilmsListComponent,
+    SpeciesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialImportsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
