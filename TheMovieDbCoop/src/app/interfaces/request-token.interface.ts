44c483960bcad7ffc18c0ba493b886d1778b9888
Alejandro <fernandez.goale21@triana.salesianos.edu>
2022-11-03 20:59:57 +0100

    falta añadir los metodos a las paginas web

---
+++ TheMovieDbCoop/src/app/interfaces/request-token.interface.ts
@@ -0,0 +1,5 @@
+export interface RequestTokenResponse{
+    success: boolean;
+    expires_at: string;
+    request_token: string;
+}
---
+++ TheMovieDbCoop/src/app/models/dto/create-session.dto.ts
@@ -0,0 +1,3 @@
+export class CreateSessionDto{
+    request_token: string = '';
+}
