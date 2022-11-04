de9562ed0922dc80d6f1190d2268b9f4bc8110e3
Alejandro <fernandez.goale21@triana.salesianos.edu>
2022-11-04 08:41:41 +0100

    añadido actor-list

--- TheMovieDbCoop/src/app/app.module.ts
+++ TheMovieDbCoop/src/app/app.module.ts
@@ -47,6 +47,9 @@ import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pa
 import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
+import { ActorListComponent } from './components/actor-list/actor-list.component';
+import { ActorDetailComponent } from './components/dialog/actor-detail/actor-detail.component';
+import { FavouriteMoviesComponent } from './components/favourite-movies/favourite-movies.component';
 
 @NgModule({
   declarations: [
@@ -84,6 +87,9 @@ import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user
     IndexComponent,
     LandingComponent,
     ProfileComponent,
+    ActorListComponent,
+    ActorDetailComponent,
+    FavouriteMoviesComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ TheMovieDbCoop/src/app/components/actor-list/actor-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actor-list',
+  templateUrl: './actor-list.component.html',
+  styleUrls: ['./actor-list.component.css']
+})
+export class ActorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
